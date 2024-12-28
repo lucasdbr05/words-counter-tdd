891ff1b12a6652397446003e0e1492068310baa9
lucasdbr05 <lucasgabrieljake@gmail.com>
2024-12-28 15:39:43 -0300

    :white_check_mark: test: add test cases related to split string in words

--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -15,15 +15,17 @@ void writeContentInInputFile(std::string s) {
 
 void clearInputFile() { writeContentInInputFile(""); }
 
-bool vectorsAreEqual(std::vector<std::string> vector1, std::vector<std::string> vector2) {
-    if((int)vector1.size() != (int)vector2.size()) return false;
-    bool flag = true;
+bool vectorsAreEqual(std::vector<std::string> vector1,
+                     std::vector<std::string> vector2) {
+  if ((int)vector1.size() != (int)vector2.size())
+    return false;
+  bool flag = true;
 
-    for(int i=0; i<5; i++) {
-        flag &= (vector1[i]==vector2[i]);
-    }
+  for (int i = 0; i < 5; i++) {
+    flag &= (vector1[i] == vector2[i]);
+  }
 
-    return flag;
+  return flag;
 }
 
 TEST_CASE("Should read input.txt file content", "[readFileContent]") {
@@ -46,7 +48,6 @@ TEST_CASE("Should read input.txt file content", "[readFileContent]") {
   clearInputFile();
 }
 
-
 TEST_CASE("Should read input.txt considering break line and spaces",
           "[readFileContent]") {
   std::string inputContent1 = "Fluminense \nFutebol Clube";
@@ -61,16 +62,33 @@ TEST_CASE("Should read input.txt considering break line and spaces",
   writeContentInInputFile(inputContent3);
   REQUIRE(inputContent3 == readFileContent());
 
-  std::string inputContent4 = "Este\n texto\n é\n o\n texto\n que será\n utilizado";
+  std::string inputContent4 =
+      "Este\n texto\n é\n o\n texto\n que será\n utilizado";
   writeContentInInputFile(inputContent4);
   REQUIRE(inputContent4 == readFileContent());
 
   clearInputFile();
 }
 
+TEST_CASE("Should split the string words and return a vector with then",
+          "[splitStringContentInWords]") {
+  std::string str1 =
+      "Sou tricolor de coração\n sou do clube tantas vezes campeão";
+  std::vector<std::string> expectedReturn1 = {
+      "Sou", "tricolor", "de",     "coração", "sou",
+      "do",  "clube",    "tantas", "vezes",   "campeão"};
+  REQUIRE(vectorsAreEqual(expectedReturn1, splitStringContentInWords(str1)));
 
-TEST_CASE("Should split the string words and return a vector with then", "[splitStringContentInWords]") {
-    std::string str = "Sou tricolor de coração\n sou do clube tantas vezes campeão";
-    std::vector<std::string> expectedReturn = {"Sou", "tricolor", "de", "coração", "sou", "do", "clube", "tantas", "vezes", "campeão"};
-    REQUIRE(vectorsAreEqual(expectedReturn, splitStringContentInWords(str)));  
+  std::string str2 = "O objetivo deste trabalho é utilizar o desenvolvimento "
+                     "orientado a testes";
+  std::vector<std::string> expectedReturn2 = {
+      "O", "objetivo",        "deste",     "trabalho", "é",     "utilizar",
+      "o", "desenvolvimento", "orientado", "a",        "testes"};
+  REQUIRE(vectorsAreEqual(expectedReturn2, splitStringContentInWords(str2)));
+
+  std::string str3 = "Este texto é o texto que será utilizado";
+  std::vector<std::string> expectedReturn3 = {
+      "Este", "texto", "é", "o", "texto", "que", "será", "utilizado",
+  };
+  REQUIRE(vectorsAreEqual(expectedReturn3, splitStringContentInWords(str3)));
 }
